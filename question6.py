f75261c3d4060c08a6113203f3db73c0526e1f5f
divyasreenivasulu-hub <84560548+divyasreenivasulu-hub@users.noreply.github.com>
2024-01-29 22:53:46 -0500

    Add files via upload

---
+++ question6.py
@@ -0,0 +1,23 @@
+def question6_1():
+
+    answer = {
+        'equal_width': ['No change', 9],
+        'equal_frequency': ['No change', 9]
+    }
+    
+    return answer
+
+def question6_2():
+    answer = {
+        'equal_width': ['change', 2],
+        'equal_frequency': ['No change', 9]
+    }
+    return answer
+
+def question6_3():
+    answer = {
+        'equal_width': ['change', 2],
+        'equal_frequency': ['change', 4]
+    }
+    return answer 
+
---
+++ question7.py
@@ -0,0 +1,13 @@
+def question7_1():
+    answer = 'increase/decrease'
+    return answer
+
+def question7_2():
+    answer = 'increase/decrease'
+    return answer
+
+def question7_3():
+    answer = [('infinity', 0.), (1., 3.), (4.,5.), (6., 'infinity')]
+    '''The representation of inifinity is 'infinity'. 
+    #bi, ei are floats or 'infinity'. '''
+    return answer 
